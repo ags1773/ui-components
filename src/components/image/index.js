98487b73e3b7c8eaeaaf01845f9a02fc2cc51b06
Amogh Sahasrabhojanee <amogh@quintype.com>
2021-05-04 17:57:54 +0530

    fix: compile esm & cjs with css

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -1,4 +1,14 @@
-import { Image } from "./image";
+import React from "react";
+import "./image.m.css";
 
-export { Image };
-export default Image;
+/**
+ * The image UI component
+ *
+ * @param {object} props
+ * @param {string} props.src image src
+ * @param {string} props.altText image alt text
+ */
+
+export function Image({ src, altText }) {
+  return <img src={src} alt={altText} styleName="image" />;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const packageJson = require("./package.json");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const config = {
+const cjsConfig = {
   mode: process.env.NODE_ENV === "development" ? "development" : "production",
   entry: path.join(__dirname, "src/components"),
   output: {
@@ -37,6 +38,41 @@ const config = {
   externals: getExternalDependancies(),
 };
 
+const esmConfig = {
+  mode: process.env.NODE_ENV === "development" ? "development" : "production",
+  entry: {
+    basket: path.join(__dirname, "src/components/basket/basket.m.css"),
+    image: path.join(__dirname, "src/components/image/image.m.css"),
+  },
+  output: {
+    filename: "esm/components/[name]/index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]__[hash:base64:5]",
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  externals: getExternalDependancies(),
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "esm/components/[name]/[name].m.css",
+    }),
+  ],
+};
+
 function getExternalDependancies() {
   const { peerDependencies, dependencies } = packageJson;
   return [
@@ -45,4 +81,7 @@ function getExternalDependancies() {
   ];
 }
 
-module.exports = config;
+module.exports = [
+  ...(process.env.BUILD_MODE === "cjs" ? [cjsConfig] : []),
+  ...(process.env.BUILD_MODE === "esm" ? [esmConfig] : []),
+];
